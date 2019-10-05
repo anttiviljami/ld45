4b358cb72966968db5e6e61fd929c39d3791b8c3
Timo Moisio <timohmoisio@gmail.com>
2019-10-05 17:38:37 +0300

    Add Spawner

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    private Transform tr;
+
+    private float initialPositionJitter = 0.1f;
+
+    [SerializeField]
+    private float clearSpaceSpeedFactor = 1;
+
+    private const float defaultClearSpaceSpeed = 10;
+
+    void Awake()
+    {
+        tr = transform;
+    }
+
+    public Vector3 Jitter()
+    {
+        return new Vector3(
+            Random.Range(-initialPositionJitter, initialPositionJitter),
+            0,
+            Random.Range(-initialPositionJitter, initialPositionJitter)
+        );
+    }
+
+    // TODO fall of edge
+}
--- Assets/Scripts/EntitySpawner.cs
+++ Assets/Scripts/EntitySpawner.cs
@@ -4,5 +4,39 @@ using UnityEngine;
 
 public class EntitySpawner : MonoBehaviour
 {
-    
+    [SerializeField]
+    private Entity defaultPrefab;
+
+    [SerializeField]
+    private Transform entitiesRoot;
+
+    [SerializeField]
+    private WorldCursor cursor;
+
+    void OnEnable()
+    {
+        SequenceDetector.NoteSequenceDetected += OnSequenceDetected;
+    }
+
+    void OnDisable()
+    {
+        SequenceDetector.NoteSequenceDetected -= OnSequenceDetected;
+    }
+
+    void Update()
+    {
+        // Test spawning
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            OnSequenceDetected(null);
+        }
+    }
+
+    private void OnSequenceDetected(Note[] noteSequence)
+    {
+        // TODO match sequence with prefab
+
+        var entity = Instantiate(defaultPrefab, cursor.Cursor.position + defaultPrefab.Jitter(), Quaternion.identity);
+        entity.transform.parent = entitiesRoot;
+    }
 }
