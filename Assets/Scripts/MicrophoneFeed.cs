7e46c3e3a3d6c0c4626f1201871b671a213012f4
Viljami Kuosmanen <viljami@avoinsorsa.fi>
2019-10-05 17:21:52 +0300

    Implemented pitch and volume detection

--- Assets/Scripts/MicrophoneFeed.cs
+++ Assets/Scripts/MicrophoneFeed.cs
@@ -4,80 +4,106 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using System.Linq;
 
 public class MicrophoneFeed : MonoBehaviour
 {
-  private static MicrophoneFeed instance;
-  public static  MicrophoneFeed Instance
-  {
-    get
+    private const int SAMPLE_FREQUENCY = 44100;
+    private const int PROCESS_SAMPLE_SIZE = 8192; // this is the maximum sample size supported for GetData
+    private const float PROCESS_INTERVAL = .1f; // 0.1 seconds
+
+    private static MicrophoneFeed instance;
+    public static MicrophoneFeed Instance
     {
-      if (instance == null)
-      {
-        var go = new GameObject("Microphone");
-        instance = go.AddComponent<MicrophoneFeed>();
-      }
+        get
+        {
+            if (instance == null)
+            {
+                var go = new GameObject("Microphone");
+                instance = go.AddComponent<MicrophoneFeed>();
+            }
 
-      return instance;
+            return instance;
+        }
     }
-  }
 
-  public static event Action ThresholdStart;
-  public static event Action<MicrophoneOutput> OutputAnalyzed;
+    public static event Action ThresholdStart;
+    public static event Action<MicrophoneOutput> OutputAnalyzed;
+    public static event Action ThresholdEnd;
 
-  public static event Action ThresholdEnd;
-
-  public struct MicrophoneOutput
-  {
-    public float pitch;
-    public float volume;
-  }
-
-  public float CurrentVolume { get; private set; }
-  public float CurrentPitch { get; private set; }
-
-  private AudioSource micAudioSource;
-
-  AudioClip microphoneInput;
-
-  void Awake()
-  {
-    // init microphone
-    if (Microphone.devices.Length > 0)
-      microphoneInput = Microphone.Start(Microphone.devices[0], true, 999, 44100);
-
-      micAudioSource = gameObject.AddComponent<AudioSource>();
-      micAudioSource.clip = microphoneInput;
-  }
-
-  /*
-   * Gets mic level ranging 0-1
-   *
-   * Source: https://www.reddit.com/r/Unity3D/comments/49wuld/best_way_to_implement_microphone_input/
-   */
-  private float GetMicrophoneLevel(float time)
-  {
-
-    // get mic volume
-    int dec = 128; // TODO factor in time
-    float[] waveData = new float[dec];
-    int micPosition = Microphone.GetPosition(null) - (dec + 1); // null means the first microphone
-    microphoneInput.GetData(waveData, micPosition);
-    micAudioSource.GetSpectrumData(waveData, 0, FFTWindow.BlackmanHarris);
-
-    // TODO magic
-
-    // get peak on the last 128 samples
-    float levelMax = 0;
-    for (int i = 0; i < dec; i++)
+    public struct MicrophoneOutput
     {
-      float wavePeak = waveData[i] * waveData[i];
-      if (levelMax < wavePeak)
-      {
-        levelMax = wavePeak;
-      }
+        public float pitch;
+        public float volume;
     }
-    float miclevel = Mathf.Sqrt(Mathf.Sqrt(levelMax));
-    return miclevel;
-  }
+
+    private AudioSource microphoneSource;
+    private AudioClip microphoneInput;
+
+    void Awake()
+    {
+        // init microphone (use first microphone available)
+        if (Microphone.devices.Length > 0)
+        {
+            microphoneInput = Microphone.Start(Microphone.devices[0], true, 30, SAMPLE_FREQUENCY);
+        }
+
+        microphoneSource = gameObject.AddComponent<AudioSource>();
+
+        InvokeRepeating("ProcessMicrophoneData", PROCESS_INTERVAL, PROCESS_INTERVAL); // process microphone data every 0.1s
+    }
+
+    void ProcessMicrophoneData()
+    {
+        // gather samples from microphone
+        float[] samples = new float[PROCESS_SAMPLE_SIZE];
+        int micPosition = Microphone.GetPosition(null) - (PROCESS_SAMPLE_SIZE + 1);
+        microphoneInput.GetData(samples, micPosition);
+
+        // create a sample clip from the sample data
+        AudioClip sample = AudioClip.Create("sample", samples.Count(), 1, SAMPLE_FREQUENCY, false);
+        sample.SetData(samples, 0);
+        sample.LoadAudioData();
+        microphoneSource.clip = sample;
+        microphoneSource.Play();
+
+        // get average level from samples
+        float sum = 0;
+        for (int i = 0; i < PROCESS_SAMPLE_SIZE; i++)
+            sum += samples[i] * samples[i]; // sum squared values
+
+        float rms = Mathf.Sqrt(sum / PROCESS_SAMPLE_SIZE); // rms = square root of average
+        float volume = Mathf.Sqrt(rms); // get volume between 0-1
+
+        // calculate pitch
+        float[] spectrum = new float[PROCESS_SAMPLE_SIZE];
+        microphoneSource.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+
+        float maxV = 0; // max amplitude
+        int maxN = 0; // index with max volume
+        for (int i = 0; i < PROCESS_SAMPLE_SIZE; i++)
+        {
+            if (spectrum[i] > maxV)
+            {
+                maxV = spectrum[i];
+                maxN = i; // maxN is the index of max
+            }
+        }
+
+        float freqN = (float)maxN;
+        if (maxN > 0 && maxN < PROCESS_SAMPLE_SIZE - 1)
+        {
+            // interpolate index using neighbours
+            var dL = spectrum[maxN - 1] / spectrum[maxN];
+            var dR = spectrum[maxN + 1] / spectrum[maxN];
+            freqN += .5f * (dR * dR - dL * dL);
+        }
+        float pitch = freqN * (SAMPLE_FREQUENCY / 2) / PROCESS_SAMPLE_SIZE;
+
+        // publish event with volume and pitch
+        var output = new MicrophoneOutput();
+        output.volume = volume;
+        output.pitch = pitch;
+        OutputAnalyzed.Invoke(output);
+    }
 }
