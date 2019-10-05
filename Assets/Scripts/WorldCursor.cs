825395b7e2e416602488523e1765be1a20717a23
Timo Moisio <timohmoisio@gmail.com>
2019-10-06 00:22:50 +0300

    Add momemtum to cursor

--- Assets/Scripts/WorldCursor.cs
+++ Assets/Scripts/WorldCursor.cs
@@ -24,19 +24,48 @@ public class WorldCursor : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 0.01f;
 
+    [SerializeField]
+    private float momentumDamping = 1;
+
+    private bool useMomentum = false;
+    private Vector2 momentum;
+
     void Awake()
     {
         instance = this;
         Cursor = transform;
     }
 
+    private void Update()
+    {
+        if (useMomentum)
+        {
+            var newPosition = Cursor.position + new Vector3(momentum.x, 0, momentum.y) * moveSpeed;
+
+            newPosition.x = Mathf.Clamp(newPosition.x, World.Instance.Bounds.xMin, World.Instance.Bounds.xMax);
+            newPosition.z = Mathf.Clamp(newPosition.z, World.Instance.Bounds.yMin, World.Instance.Bounds.yMax);
+
+            Cursor.position = newPosition;
+
+            momentum = Vector2.MoveTowards(momentum, Vector2.zero, Time.deltaTime * momentumDamping);
+        }
+        else if (momentum.sqrMagnitude > 0.1f)
+        {
+            useMomentum = true;
+        }
+    }
+
     public void Move(Vector2 amount)
     {
+        useMomentum = false;
+
         var newPosition = Cursor.position + new Vector3(amount.x, 0, amount.y) * moveSpeed;
 
         newPosition.x = Mathf.Clamp(newPosition.x, World.Instance.Bounds.xMin, World.Instance.Bounds.xMax);
         newPosition.z = Mathf.Clamp(newPosition.z, World.Instance.Bounds.yMin, World.Instance.Bounds.yMax);
 
         Cursor.position = newPosition;
+
+        momentum = amount;
     }
 }
