90ae31770711120052877b1cfb2f51dee26e98d9
Viljami Kuosmanen <viljami@avoinsorsa.fi>
2019-10-06 23:05:45 +0300

    More achievements

--- Assets/Scripts/AchievementsController.cs
+++ Assets/Scripts/AchievementsController.cs
@@ -31,6 +31,7 @@ public class AchievementsController
         WolfAttack,
         OffTheEdge,
         Murder,
+        Tornado,
         Vainamoinen,
         FourSeasons,
     }
@@ -38,8 +39,9 @@ public class AchievementsController
     public Dictionary<Achievement, string> achievements = new Dictionary<Achievement, string>() {
         { Achievement.LetThereBeLight, "Let there be light" },
         { Achievement.WolfAttack, "Wolf Attack" },
+        { Achievement.Tornado, "Tornado season" },
         { Achievement.OffTheEdge, "Fall off the edge" },
-        { Achievement.Murder, "Murder most foul" },
+        { Achievement.Murder, "A murder most foul" },
         { Achievement.Vainamoinen, "Vainamoinen" },
         { Achievement.FourSeasons, "Four seasons" },
     };
@@ -66,16 +68,34 @@ public class AchievementsController
     {
         TriggerAchivement(Achievement.LetThereBeLight);
 
+        // check swamp
         if (noteSequence.note1.NoteName == Note.Name.Earth
         && noteSequence.note2.NoteName == Note.Name.Weather
         && noteSequence.note3.NoteName == Note.Name.Weather)
         {
             TriggerAchivement(Achievement.Vainamoinen);
         }
+
+        // check tornado
+        if (noteSequence.note1.NoteName == Note.Name.Weather
+        && noteSequence.note2.NoteName == Note.Name.Weather
+        && noteSequence.note3.NoteName == Note.Name.Weather)
+        {
+            TriggerAchivement(Achievement.Tornado);
+        }
     }
 
     public void OnEntityKilled(KillEventArgs args)
     {
+        // check wolf
+        if (args.killer.recipe.note1.NoteName == Note.Name.Animals
+        && args.killer.recipe.note2.NoteName == Note.Name.Earth
+        && args.killer.recipe.note3.NoteName == Note.Name.Weather
+        )
+        {
+            TriggerAchivement(Achievement.WolfAttack);
+        }
+
         // check barbarian
         if (args.killer.recipe.note1.NoteName == Note.Name.Animals
         && args.killer.recipe.note2.NoteName == Note.Name.Animals
