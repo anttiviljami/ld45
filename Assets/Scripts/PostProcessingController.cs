3ce0bf6972b870abd37653a82e294a2d8d9b44ec
Timo Moisio <timohmoisio@gmail.com>
2019-10-06 10:01:02 +0300

    Add volume based lens distortion

---
+++ Assets/Scripts/PostProcessingController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[RequireComponent(typeof(PostProcessVolume))]
+public class PostProcessingController : MonoBehaviour
+{
+    [SerializeField]
+    private AnimationCurve distortionByVolume;
+
+    private PostProcessVolume volume;
+
+    private LensDistortion distortion;
+
+    void OnEnable()
+    {
+        volume = GetComponent<PostProcessVolume>();
+        MicrophoneFeed.OutputAnalyzed += MicrophoneFeed_OutputAnalyzed;
+    }
+
+    void OnDisable()
+    {
+        MicrophoneFeed.OutputAnalyzed -= MicrophoneFeed_OutputAnalyzed;
+    }
+
+    void MicrophoneFeed_OutputAnalyzed(MicrophoneFeed.MicrophoneOutput output)
+    {
+        var distortionAmount = distortionByVolume.Evaluate(output.volume);
+
+        if (volume.profile.TryGetSettings(out distortion))
+        {
+            if (distortionAmount > 0)
+            {
+                distortion.enabled.value = true;
+                distortion.intensity.value = -distortionAmount;
+            }
+            else
+            {
+                distortion.enabled.value = false;
+            }
+        }
+    }
+}
